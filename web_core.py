fb160c598ac34a8bf555f7e12ba6a0e2eeecd26d
Ermyshev Maxim (Ural) <maksim.ermyshev@Megafon.ru>
2020-03-17 19:14:14 +0500

    first commit

---
+++ web_core.py
@@ -0,0 +1,54 @@
+#!flask/bin/python
+from flask import Flask
+from flask import request
+import datetime
+
+app = Flask(__name__)
+
+
+# curl -X POST -H "Content-Type: application/json; charset=UTF-8" http://10.66.8.21:5000/api_get_time
+@app.route("/api_get_time", methods=["POST"])
+def api_get_time():
+    return "Current date and time: " + str(datetime.datetime.now()) + "\n"
+
+#print
+
+# curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data '{"a":"5","b":"10","operation":"+"}' http://10.66.8.21:5000/api_get_calculate
+@app.route("/api_get_calculate", methods=["POST"])
+def api_get_calculate():
+    if request.is_json:
+        a = request.json.get("a")
+        b = request.json.get("b")
+        operation = request.json.get("operation")
+        return self_calculator(a, b, operation) + "\n"
+    else:
+        return "No input JSON data"
+
+
+def self_calculator(a, b, operation):
+    if operation == '+':
+        return str(int(a) + int(b));
+    if operation == '-':
+        return str(int(a) - int(b));
+    if operation == '*':
+        return str(int(a) * int(b));
+    if operation == '/':
+        if (b == '0'):
+            return "Infinity"
+        else:
+            return str(int(a) / int(b));
+
+
+# curl -X POST -H "Content-Type: application/json; charset=UTF-8" http://10.66.8.21:5000/api_get_help
+@app.route("/api_get_help", methods=["POST"])
+def api_get_help():
+    curl_command = "use this curl command to check my project:\n" \
+                   "curl -X POST -H \"Content-Type: application\/json; charset=UTF-8\" (optional) --data '{JSON DATA}' http://x.x.x.x:port/method\n"
+    use_help_get_time = "api_get_time - use POST request without params to get current time\n"
+    use_help_get_calculate = "api_get_calculate - use follow JSON format {\"a\": your_data ,\"b\": your_data,\"operation\": operation above digits} " \
+                             "[example: \'{\"a\":\"5\",\"b\":\"10\",\"operation\":\"+\"}\']\n"
+    use_help_get_help = "api_get_help - use POST request without params to get HELP\n"
+    return curl_command + "\nAPI:\n" + use_help_get_time + use_help_get_calculate + use_help_get_help
+
+
+app.run(host='0.0.0.0', port=5000)
